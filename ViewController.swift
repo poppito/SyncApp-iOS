5adca565c9bb7f5e745224fb83f098b14a8fd2ae
Harsh Overseer <harsh@embry.io>
2017-02-05 17:16:52 +1100

    first commit to empty repo

---
+++ ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Syncapp-iOS
+//
+//  Created by crabsillon pawson on 26/1/17.
+//  Copyright © 2017 embryio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK: outlets
+    
+    @IBOutlet weak var textBox: UITextField!
+    
+    
+    var currentTextState = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        textBox.delegate = self
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: actions
+    
+    @IBAction func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.resignFirstResponder()
+    }
+    
+    @IBAction func pressButton(_ sender: UIButton) {
+        if (currentTextState) {
+            textBox.text = "hello"
+            currentTextState = false
+        }
+        else {
+            textBox.text = "world"
+            currentTextState = true
+        }
+    }
+
+}
+
